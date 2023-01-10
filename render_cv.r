550a2e53bc2a7a8d352b97539c97d8a732709635
lisahaxel <117662813+lisahaxel@users.noreply.github.com>
2023-01-10 20:32:13 +0100

    Add files via upload

---
+++ render_cv.r
@@ -0,0 +1,19 @@
+# This script builds both the HTML and PDF versions of your CV
+
+# Knit the HTML version
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "index.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "haxel_cv.pdf",
+                       extra_args = "--no-sandbox"
+)
+# %%
